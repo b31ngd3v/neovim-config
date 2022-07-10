77bda42231d2644c121fa26e892b1e1be9b1b18b
b31ngd3v <contact@b31ngd3v.eu.org>
2022-07-10 15:39:43 +0530

    fix: change added, modified and deleted icons

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -20,7 +20,7 @@ local diagnostics = {
 local diff = {
     "diff",
     colored = false,
-    symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+    symbols = { added = "A ", modified = "M ", removed = "D " }, -- changes diff symbols
     cond = hide_in_width
 }
 
