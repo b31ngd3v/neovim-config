f6e4790e62b467e4d5db5708cd22273428c58dc1
b31ngd3v <contact@b31ngd3v.eu.org>
2022-09-19 23:14:33 +0530

    fix: tokyonight theme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,4 @@
-local colorscheme = "tokyonight"
-vim.g.tokyonight_style = "night"
+local colorscheme = "tokyonight-night"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
