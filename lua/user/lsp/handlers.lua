3adebbe91583e648d1dd5dfe049b7948b4746905
b31ngd3v <contact@b31ngd3v.eu.org>
2022-10-02 12:20:03 +0530

    fix: multiple breaking changes

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -81,7 +81,7 @@ end
 
 M.on_attach = function(client, bufnr)
   if client.name == "tsserver" or client.name == "jsonls" or client.name == "html" then
-    client.resolved_capabilities.document_formatting = false
+    client.server_capabilities.document_formatting = false
   end
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
